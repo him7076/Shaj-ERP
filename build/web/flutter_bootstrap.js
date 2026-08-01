7ff133fbf0fc16ba659a4e91f27f5428bace9494
him7076 <98527599+him7076@users.noreply.github.com>
2026-08-01 11:47:30 +0000

    chore: Auto-build web bundle for Vercel deployment

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1874538888" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "303368455" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
