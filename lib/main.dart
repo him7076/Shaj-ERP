e209219420f436b665f0f1d3a802d20161f823fc
himanshu <himanshuchandwani2363@gmail.com>
2026-08-22 00:26:17 +0530

    fix: add missing flutter/services.dart import for MethodChannel in main.dart to resolve Vercel web build error

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_core/firebase_core.dart';
